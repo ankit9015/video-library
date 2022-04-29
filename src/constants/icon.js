65064b0903b897ca4a932e33d5cc13738deb9b5e
Ankit Joshi <67504902+ankit9015@users.noreply.github.com>
2022-04-29 22:27:57 +0530

    feat: header-structure-updated

---
+++ src/constants/icon.js
@@ -0,0 +1,25 @@
+import {
+  MdDarkMode,
+  MdLightMode,
+  MdSearch,
+  MdHome,
+  MdHistory,
+  MdOutlineWatchLater,
+  MdMenu,
+} from "react-icons/md";
+
+import { FaRegUserCircle, FaFilter, FaEye, FaEyeSlash } from "react-icons/fa";
+
+export {
+  FaRegUserCircle,
+  FaFilter,
+  FaEye,
+  FaEyeSlash,
+  MdLightMode,
+  MdDarkMode,
+  MdSearch,
+  MdHome,
+  MdHistory,
+  MdOutlineWatchLater,
+  MdMenu,
+};
--- src/pageLayout/Header.js
+++ src/pageLayout/Header.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { MdMenu, FaRegUserCircle } from "../constants/icon";
+import "./pageLayout.css";
 
 function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header flex-row  flex-align-center p-xs">
+      <div className="flex-row  flex-align-center gap-5">
+        <span className="header__menu-button text-xl icon-button">
+          <MdMenu />
+        </span>
+        <span className="header__logo text-md m-xs">Logo</span>
+      </div>
+      <span className="header__search-box">searchbox</span>
+      <div className="flex-row  flex-align-center gap-5">
+        <button className="header__login text-md">Login</button>
+        <div className=" header__avatar avatar m-xs avatar-text">
+          {/* <span className="text-lg">G</span> */}
+          <FaRegUserCircle />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
